6016855a960b825940f06ee7cc9e7c05a3786c1b
SHUBHAM KATIYAR <168412256+ShubhamKatiyar780@users.noreply.github.com>
2025-04-27 09:26:08 +0530

    Add File

---
+++ literals.py
@@ -0,0 +1,30 @@
+a = 0b1010 # binary literals
+b = 100 # decimal literals
+c = 0o310 # octal literals
+d = 0x12c # hexadecimal literals
+
+# float literals
+f1 = 10.5
+f2 = 1.5e2  # 1.5 * 10^2
+f3 = 1.9e-3  # 1.9 * 10^-3
+
+# complex literals
+x = 4+3j
+print(x.real, x.imag)
+
+# string literals
+s1 = 'Shubham'
+s2 =  "Katiyar"
+s3 = '''This is also a string'''
+unicode = u"\U0001f600\U0001f606\U0001f923"
+raw_string = r"ram.\n.string"
+print(s1)
+print(s2)
+print(s3)
+print(unicode)
+print(raw_string)
+
+# boolean literals
+a1 = True + 5
+b1 = False + 5
+print('a1:',a1,'b1:',b1)
