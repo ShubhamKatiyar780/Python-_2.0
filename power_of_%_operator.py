69c43313ce1e41bb5c89bf8401a5fc6996f1c2ad
SHUBHAM KATIYAR <168412256+ShubhamKatiyar780@users.noreply.github.com>
2025-06-10 14:08:42 +0530

    a

--- power_of_%_operator.py
+++ power_of_%_operator.py
@@ -2,23 +2,23 @@
 # Basic Syntax: "format string" % (value1, value2, ...)
 
 
-# ✅ Example 1: Single value
+# Example 1: Single value
 name = "Shubham"
 print("Hello %s" % name)
 # Output: Hello Shubham
 
-# ✅ Example 2: Multiple values
+# Example 2: Multiple values
 name = "Shubham"
 age = 25
 print("My name is %s and I am %d years old." % (name, age))
 # Output: My name is Shubham and I am 25 years old.
 
-# ✅ Example 3: Float formatting
+# Example 3: Float formatting
 price = 49.9999
 print("Price: %.2f" % price)
 # Output: Price: 50.00
 
-# ✅ Example 3: Octal formatting
+# Example 3: Octal formatting
 num = 15
 print("Octal: %o" % num)
 # Output: Octal: 17
