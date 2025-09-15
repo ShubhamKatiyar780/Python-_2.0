3c1c8c7ea5d141b86f17ac84a8105c134d109530
SHUBHAM KATIYAR <168412256+ShubhamKatiyar780@users.noreply.github.com>
2025-09-15 21:35:33 +0530

    kwargs

--- Function in Python/with_kwargs.py
+++ Function in Python/with_kwargs.py
@@ -1,10 +1,10 @@
 # Define a function that accepts any number of keyword arguments
 def fun(**kwargs):
     # Iterate over the dictionary of keyword arguments
-    for k, val in kwargs.items():
+    for key, val in kwargs.items():
         # Print each key-value pair
-        print(f"{k}: {val}")
+        print(f"{key}: {val}")
 
 # Calling the function with keyword arguments
-# These arguments are passed as a dictionary: {"name": "Alice", "age": 30, "city": "New York"}
+# These arguments are passed as a dictionary: {"name"="Shubham Katiayr", 'age'=24, 'city'="Kanpur"}
 fun(name="Shubham Katiayr", age=24, city="Kanpur")
